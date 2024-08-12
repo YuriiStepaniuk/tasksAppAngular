f6f4b52ebe141f5e0e1bd2e0842b2ce81d99c5f5
Yurii <stepanuk.ura2001@gmail.com>
2024-08-12 20:56:34 +0300

    finishing touches

--- src/app/tasks/task/task.component.ts
+++ src/app/tasks/task/task.component.ts
@@ -1,7 +1,8 @@
-import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { Task } from './task.model';
 import { DatePipe } from '@angular/common';
 import { CardComponent } from '../../ui/card/card.component';
+import { TasksService } from '../tasks.service';
 
 @Component({
   selector: 'app-task',
@@ -12,9 +13,10 @@ import { CardComponent } from '../../ui/card/card.component';
 })
 export class TaskComponent {
   @Input({ required: true }) task!: Task;
-  @Output() complete = new EventEmitter<string>();
+
+  constructor(private tasksService: TasksService) {}
 
   onCompleteTask() {
-    this.complete.emit(this.task.id);
+    this.tasksService.removeTask(this.task.id);
   }
 }
--- src/app/tasks/tasks.component.ts
+++ src/app/tasks/tasks.component.ts
@@ -3,6 +3,7 @@ import { TaskComponent } from './task/task.component';
 import { DUMMY_TASKS } from './dummy-tasks';
 import { NewTaskComponent } from './new-task/new-task.component';
 import { NewTaskData } from './task/task.model';
+import { TasksService } from './tasks.service';
 
 @Component({
   selector: 'app-tasks',
@@ -16,32 +17,27 @@ export class TasksComponent {
   @Input({ required: true }) userName!: string | undefined;
   isAddingTask = false;
 
+  constructor(private tasksService: TasksService) {}
+
   tasks = DUMMY_TASKS;
 
   get selectedUserTasks() {
-    return this.tasks.filter((task) => task.userId === this.id);
+    return this.tasksService.getUserTasks(this.id)
   }
 
   onCompleteTask(id: string) {
-    this.tasks = this.tasks.filter((task) => task.id !== id);
+    
   }
 
   onStartAddTasks() {
     this.isAddingTask = true;
   }
 
-  onCancelAddTask() {
+  onCloseAddTask() {
     this.isAddingTask = false;
   }
 
   onAddTask(taskData: NewTaskData) {
-    this.tasks.push({
-      id: new Date().getTime().toString(),
-      userId: this.id,
-      title: taskData.title,
-      summary: taskData.summary,
-      dueDate: taskData.date,
-    });
     this.isAddingTask = false;
   }
 }
